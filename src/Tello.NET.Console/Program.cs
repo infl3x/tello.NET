3d6495ca515a8298f43876d444f43c0e0d2f866d
Nick Ellercamp <nick.ellercamp@cint.com>
2018-12-05 10:25:37 +1100

    Initial commit

---
+++ src/Tello.NET.Console/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using CommandLine = System.Console;
+
+namespace Tello.NET.Console
+{
+	public class Program
+	{
+		private static readonly IPAddress DefaultIp = IPAddress.Parse("192.168.10.1");
+		private static readonly int DefaultPort = 8889;
+
+		public static void Main(string[] args)
+		{
+			CommandLine.WriteLine($"Enter Tello IPv4 address ({DefaultIp}): ");
+			IPAddress ipAddress = ReadAndParse(l => IPAddress.Parse(l), DefaultIp);
+
+			CommandLine.WriteLine($"Enter Tello port ({DefaultPort}): ");
+			int port = ReadAndParse(l => Int32.Parse(l), DefaultPort);
+
+			CommandLine.WriteLine("Attempting to connect...");
+
+			try
+			{
+				ITelloClient telloClient = Connect(ipAddress, port);
+
+				string command = null;
+				do
+				{
+					CommandLine.WriteLine("Enter command:");
+					command = CommandLine.ReadLine();
+
+					telloClient.Send(command);
+
+				} while (!String.IsNullOrEmpty(command));
+			}
+			catch (Exception e)
+			{
+				CommandLine.WriteLine($"An error has occurred: ${e}");
+			}
+
+			CommandLine.ReadLine();
+		}
+
+		private static ITelloClient Connect(IPAddress ipAddress, int port)
+		{
+			ITelloClient telloClient = new TelloClient(ipAddress, port);
+			telloClient.Connect();
+
+			Task.Factory.StartNew(() => telloClient.ListenForResponses(OnResponseReceived));
+
+			CommandLine.WriteLine("Connected successfully!");
+
+			return telloClient;
+		}
+
+		private static T ReadAndParse<T>(Func<string, T> parse, T defaultValue)
+		{
+			string line = CommandLine.ReadLine();
+
+			if (String.IsNullOrEmpty(line))
+				return defaultValue;
+
+			while (true)
+			{
+				try
+				{
+					return parse(line);
+				}
+				catch (Exception e)
+				{
+					CommandLine.WriteLine(e.Message);
+					CommandLine.WriteLine("Please try enter data in the correct format");
+				}
+			}
+		}
+
+		private static void OnResponseReceived(string response)
+		{
+			CommandLine.WriteLine("Response recieved: ");
+			CommandLine.WriteLine(response);
+		}
+	}
+}
---
+++ src/Tello.NET/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tello.NET
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
